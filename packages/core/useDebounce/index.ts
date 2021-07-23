f002690c7441ca15e71a0e244509c198ce7de119
Arpit Bharti <arpitbharti73@gmail.com>
2021-07-24 01:16:14 +0530

    Added packages and useDebounce as example

---
+++ packages/core/useDebounce/index.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+
+export const useDebounce = <T>(value: T, timeout: number): T => {
+  // Save a local copy of `value` in this state which is local to our hook
+  const [state, setState] = useState(value)
+
+  useEffect(() => {
+    // Set timeout to run after delay
+    const handler = setTimeout(() => setState(value), timeout)
+
+    // clear the setTimeout listener on unMount
+    return () => clearTimeout(handler)
+  }, [value, timeout])
+
+  return state
+}

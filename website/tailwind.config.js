a080970d853f393d244bdae2892b1bd9fd731c52
Arpit Bharti <arpitbharti73@gmail.com>
2021-07-26 01:40:42 +0530

    Separate website into different worskspace, configs upddated accrodigly

---
+++ website/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/pages/**/*.{jsx,tsx}', './packages/**/*.{jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}

6c1f53256510aa0cdf2b4d3cfc30cde632325ec6
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-01 23:39:29 +0530

    Added demo and docs for usePrevious

---
+++ packages/core/usePrevious/demo.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import { usePrevious } from '.'
+
+export function Demo() {
+  const [count, setCount] = useState(0)
+  const previousCount = usePrevious(count)
+
+  return (
+    <div>
+      <div>Current Count is - {count}</div>
+      <div>Previous Count is - {previousCount}</div>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+    </div>
+  )
+}

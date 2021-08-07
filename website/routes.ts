434c3eedfcae2295f5b540361e9bc9df1cb55f6e
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-08 00:17:39 +0530

    Created a routes building script

---
+++ website/routes.ts
@@ -0,0 +1,16 @@
+// This is a auto generated file. Do not modify
+
+export type Route = { name: string; category: string; route: string }
+
+export const routes: Route[] = [
+  {
+    "name": "useDebounce",
+    "category": "core",
+    "route": "/core/useDebounce"
+  },
+  {
+    "name": "usePrevious",
+    "category": "core",
+    "route": "/core/usePrevious"
+  }
+]
---
+++ website/scripts/buildRoutes.ts
@@ -0,0 +1,26 @@
+import fs from 'fs'
+import { join } from 'path'
+
+import { getAllFunctionsMeta } from '../utils/loadMDX'
+
+const indexPath = join(__dirname, '..', 'routes.ts')
+
+async function buildRoutes() {
+  const functionMeta = (await getAllFunctionsMeta()).map(
+    ({ name, pkg, category }) => ({ name, category, route: `/${pkg}/${name}` })
+  )
+
+  const source = [
+    `// This is a auto generated file. Do not modify\n\n`,
+
+    `export type Route = { name: string; category: string; route: string }\n\n`,
+    `export const routes: Route[] = `,
+    JSON.stringify(functionMeta, null, 2)
+  ].join('')
+
+  fs.writeFileSync(indexPath, source, {
+    encoding: 'utf-8'
+  })
+}
+
+buildRoutes()

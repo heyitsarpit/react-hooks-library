0e31fc8dd30b17075833b16b6c423f4215c2dc41
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-08 10:32:42 +0530

    Added polka background to playground

--- website/ui/docs/Playground.tsx
+++ website/ui/docs/Playground.tsx
@@ -4,7 +4,8 @@ type Props = { children: React.ReactNode }
 
 export function Playground({ children }: Props) {
   return (
-    <div className="relative playground w-screen md:w-[var(--width)] ml-[var(--margin-left)] p-12 bg-fg-1 top-[-2rem]">
+    <div className="relative border-b border-b-coolGray-200 dark:border-b-coolGray-900 polka playground w-screen md:w-[var(--width)] ml-[var(--margin-left)] p-12 top-[-2rem]">
+      <div className="absolute opacity-30 top-2 left-2">DEMO</div>
       {children}
     </div>
   )

bea0e5ea48fa31353bf1e88c2b2472e99b405f7a
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-08 23:37:47 +0530

    Animate active tab marker in function list in sidebar

--- website/ui/FunctionList.tsx
+++ website/ui/FunctionList.tsx
@@ -1,5 +1,6 @@
-import { useRouter } from 'next/dist/client/router'
+import { AnimateSharedLayout, motion } from 'framer-motion'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import type { Route } from '../routes'
 import { routes } from '../routes'
@@ -9,35 +10,58 @@ const groupedRoutes: {
 } = {}
 
 routes.forEach((route) => {
-  if (groupedRoutes[route.category]) {
-    groupedRoutes[route.category].push(route)
-  } else {
-    groupedRoutes[route.category] = [route]
-  }
+  groupedRoutes[route.category]
+    ? groupedRoutes[route.category].push(route)
+    : (groupedRoutes[route.category] = [route])
 })
+
+type ItemProps = {
+  name: string
+  route: string
+  isActive: boolean
+}
+
+function ListItem({ name, route, isActive }: ItemProps) {
+  return (
+    <li className="relative flex items-center w-full py-1 pl-0 before:hidden">
+      {isActive ? (
+        <motion.div
+          layoutId="active"
+          className="absolute w-full h-full active pill"
+        />
+      ) : null}
+      <Link href={route} passHref>
+        <a
+          className={`block w-full pl-4 hover:text-brand hover:no-underline ${
+            isActive ? 'font-semibold' : ''
+          }`}>
+          {name}
+        </a>
+      </Link>
+    </li>
+  )
+}
 
 export function FunctionList() {
   const router = useRouter()
-  const isActive = (route: string) =>
-    router.asPath.includes(route) ? 'active rounded-lg font-semibold' : ''
+  const isActive = (route: string) => router.asPath.includes(route)
 
   return (
-    <>
-      {Object.keys(groupedRoutes).map((category) => (
-        <ul className="list-none" key={category}>
-          {groupedRoutes[category].map(({ name, route }) => (
-            <li
-              className={`w-full py-1 before:hidden ${isActive(route)}`}
-              key={route}>
-              <Link href={route}>
-                <a className="block hover:text-brand hover:no-underline">
-                  {name}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ))}
-    </>
+    <AnimateSharedLayout>
+      <motion.div layout>
+        {Object.keys(groupedRoutes).map((category) => (
+          <ul className="list-none" key={category}>
+            {groupedRoutes[category].map(({ name, route }) => (
+              <ListItem
+                key={name}
+                name={name}
+                route={route}
+                isActive={isActive(route)}
+              />
+            ))}
+          </ul>
+        ))}
+      </motion.div>
+    </AnimateSharedLayout>
   )
 }

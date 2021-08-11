04e29186128efac08e9c6279c8cd3c3063589cb3
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-11 08:19:12 +0530

    Added github link

--- website/ui/Header.tsx
+++ website/ui/Header.tsx
@@ -1,5 +1,32 @@
 import { ThemeSwitch } from './ThemeSwitch'
 
+function Github() {
+  return (
+    <div className="hover:text-brand">
+      <a
+        className="flex items-center text-current hover:no-underline"
+        href="https://github.com/react-hooks-library/react-hooks-library"
+        target="_blank"
+        rel="noopener noreferrer">
+        Github
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-4 h-4 ml-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+          />
+        </svg>
+      </a>
+    </div>
+  )
+}
+
 export function Header() {
   return (
     <header className="fixed top-0 bottom-0 left-0 right-0 z-10 h-[var(--header-height)] border-b bg-bg-2 border-b-fg-1 flex items-center">
@@ -10,6 +37,7 @@ export function Header() {
           alt="React Hooks Library Logo"
           className="py-2 ml-2 h-[var(--header-height)]"
         />
+        <Github />
         <ThemeSwitch />
       </div>
     </header>

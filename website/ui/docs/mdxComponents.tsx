26c49886ecaa8e929c2318e9aa56de89ca137b28
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-11 14:32:23 +0530

    Styling changes

--- website/ui/docs/mdxComponents.tsx
+++ website/ui/docs/mdxComponents.tsx
@@ -1,4 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import Link from 'next/link'
+
 import { Playground } from './Playground'
 import { Source } from './Source'
 
-export const mdxComponents = { Playground, Source }
+export const mdxComponents = {
+  Playground,
+  Source,
+  a({ href = '', ...props }) {
+    return (
+      <Link href={href} passHref>
+        <a {...props} />
+      </Link>
+    )
+  },
+  p(props) {
+    return <p className="my-6" {...props} />
+  }
+}

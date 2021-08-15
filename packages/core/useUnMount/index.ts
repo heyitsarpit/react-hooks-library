4f81d6123b83314c40c0302af6e1dc3bd67e2cf8
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-15 23:07:32 +0530

    Added useMount hook - funcyion | docs

---
+++ packages/core/useUnMount/index.ts
@@ -0,0 +1,13 @@
+import { Fn } from '@react-hooks-library/shared'
+import { useEffect } from 'react'
+
+/**
+ * Run a function when component is unmounted.
+ *
+ * @param callback function to be executed
+ */
+export function useUnMount(func: Fn) {
+  useEffect(() => {
+    return func
+  }, [])
+}

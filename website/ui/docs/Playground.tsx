74ad016223eb1fe521a85293e15c600992d464f5
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-22 23:15:09 +0530

    Update playground to have min-height

--- website/ui/docs/Playground.tsx
+++ website/ui/docs/Playground.tsx
@@ -4,7 +4,7 @@ type Props = { children: React.ReactNode }
 
 export function Playground({ children }: Props) {
   return (
-    <div className="bleed h-[16rem] polka p-12 top-[-2rem] border-b border-b-coolGray-200 dark:border-b-coolGray-900">
+    <div className="bleed min-h-[16rem] polka p-12 top-[-2rem] border-b border-b-coolGray-200 dark:border-b-coolGray-900">
       <div className="absolute opacity-30 top-2 right-5">DEMO</div>
       <div className="flex items-center justify-center h-full">{children}</div>
     </div>

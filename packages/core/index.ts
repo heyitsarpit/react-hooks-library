4849624c69390d117ea610adf40f45ba1a17822d
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-22 22:04:11 +0530

    Update core package exports

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,8 +1,10 @@
 // This file is auto generated. Do not edit manually.
 
+export * from './BreakPointHooks'
 export * from './useActiveElement'
 export * from './useDebounce'
 export * from './useEventListener'
+export * from './useMediaQuery'
 export * from './useMount'
 export * from './useMountSync'
 export * from './usePrevious'

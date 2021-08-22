7e4f21a43f1e2ffbc09fe9a420bb92679e25ada0
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-23 00:31:41 +0530

    Fix overflow issues in Playground

--- website/ui/docs/Playground.tsx
+++ website/ui/docs/Playground.tsx
@@ -4,9 +4,11 @@ type Props = { children: React.ReactNode }
 
 export function Playground({ children }: Props) {
   return (
-    <div className="bleed min-h-[16rem] polka p-12 top-[-2rem] border-b border-b-coolGray-200 dark:border-b-coolGray-900">
+    <div className="bleed min-h-[16rem] polka px-12 py-2 top-[-2rem] border-b border-b-coolGray-200 dark:border-b-coolGray-900">
       <div className="absolute opacity-30 top-2 right-5">DEMO</div>
-      <div className="flex items-center justify-center h-full">{children}</div>
+      <div className="flex items-center justify-center min-h-[inherit]">
+        {children}
+      </div>
     </div>
   )
 }

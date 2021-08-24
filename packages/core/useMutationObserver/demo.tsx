2f958668618315bf5b189f0bb1cd9af0b8a475e6
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-24 23:58:05 +0530

    Update Demo for useMutationObserver

--- packages/core/useMutationObserver/demo.tsx
+++ packages/core/useMutationObserver/demo.tsx
@@ -1,17 +1,43 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
+import { useUnMount } from '../useUnMount'
 import { useMutationObserver } from '.'
 
 export function Demo() {
-  const ref = useRef<HTMLDivElement | null>(null)
+  const ref = useRef<HTMLButtonElement | null>(null)
+  const [observed, setObserved] = useState(false)
 
   useMutationObserver(
     ref,
-    () => {
-      console.log('useMutationObserver')
+    (mutations) => {
+      for (const mutation of mutations) {
+        if (mutation.type === 'attributes') {
+          setObserved(true)
+        }
+      }
     },
     { attributes: true }
   )
 
-  return <div ref={ref}>Hello World</div>
+  useUnMount(stop)
+
+  const addAttribute = () => {
+    if (!ref.current) return
+
+    ref.current.setAttribute('data-mut', 'hello world')
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="mb-4">
+        {observed
+          ? 'Observed attribute change to node'
+          : 'No changes observed yet'}
+      </div>
+
+      <button ref={ref} onClick={addAttribute} disabled={observed}>
+        {observed ? 'Added Attribute To Node' : 'Add Attribute To Node'}
+      </button>
+    </div>
+  )
 }

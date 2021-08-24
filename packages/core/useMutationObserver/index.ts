4313bacbf3a27704f507ce34bc3a1036b963bedc
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-24 23:58:31 +0530

    Update jsdoc for useMutationObserver

--- packages/core/useMutationObserver/index.ts
+++ packages/core/useMutationObserver/index.ts
@@ -6,9 +6,9 @@ import { useUnMount } from '../useUnMount'
 /**
  * Watch for changes being made to the DOM tree.
  *
- * @param target
- * @param callback
- * @param options
+ * @param target - React ref or DOM node
+ * @param callback - callback to execute when mutations are observed
+ * @param options - Options passed to mutation observer
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver MutationObserver MDN
  * @see https://react-hooks-library.vercel.app/core/useMutationObserver
@@ -45,5 +45,8 @@ export function useMutationObserver(
     return stop
   }, [callback, stop, options, target])
 
-  return { isSupported: isSupported.current, stop }
+  return {
+    isSupported: isSupported.current,
+    stop
+  }
 }

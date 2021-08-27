f6ca5098dee3636285058b980546db8efd9663d3
Arpit Bharti <arpitbharti73@gmail.com>
2021-08-27 23:31:39 +0530

    Add pwa dependencies

--- website/next.config.js
+++ website/next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    runtimeCaching
+  },
   experimental: {
     externalDir: true
   }
-}
+})

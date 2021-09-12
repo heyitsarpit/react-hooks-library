462a5429985fd7e3a1095bae4cc65212b4b00c09
Arpit Bharti <arpitbharti73@gmail.com>
2021-09-12 20:34:31 +0530

    Added useNetwork demo

---
+++ packages/core/useNetwork/demo.tsx
@@ -0,0 +1,33 @@
+import { isNumber } from '@react-hooks-library/shared'
+
+import { NetworkEffectiveType, useNetwork } from '.'
+
+export function Demo() {
+  const network = useNetwork()
+
+  const getClass = (
+    value: number | boolean | NetworkEffectiveType | ConnectionType
+  ) =>
+    `pill ${
+      value === undefined
+        ? 'opacity-60'
+        : value || isNumber(value)
+        ? 'active'
+        : 'danger'
+    }`
+
+  return (
+    <div>
+      <dl className="my-4">
+        {Object.entries(network).map(([key, value]) => (
+          <div
+            key={`${key} + ${value}`}
+            className="flex items-center gap-4 my-2">
+            <dt>{key}:</dt>
+            <dd className={getClass(value)}>{`${value}` ?? 'unknown'}</dd>
+          </div>
+        ))}
+      </dl>
+    </div>
+  )
+}

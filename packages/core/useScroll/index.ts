f22c4f033bcf1e31d35c1e6950f4e1a6be39faa8
Arpit Bharti <arpitbharti73@gmail.com>
2021-09-25 17:02:49 +0530

    Updated useScroll hook api to not re-render on scroll, major performance optimisation

--- packages/core/useScroll/index.ts
+++ packages/core/useScroll/index.ts
@@ -1,46 +1,43 @@
 import { MaybeRef, round, unRef } from '@react-hooks-library/shared'
-import { useState } from 'react'
 
 import { _document } from '../_ssr.config'
 import { useEventListener } from '../useEventListener'
-import { useMount } from '../useMount'
 
 /**
  * Reactive scroll values for a react ref or a dom node
  *
  * @param target - dom node or react ref
+ * @param callback - callback to run on scroll
  *
  * @see https://react-hooks-library.vercel.app/core/useScroll
  */
 export function useScroll(
-  target: MaybeRef<Element | null | undefined> = _document?.documentElement
+  target: MaybeRef<Element | null | undefined> = _document?.documentElement,
+  callback: (coords: { scrollX: number; scrollY: number }) => void
 ) {
-  const [x, setX] = useState(0)
-  const [y, setY] = useState(0)
-
-  const setPositions = () => {
+  const getPositions = () => {
     const el = unRef(target)
     if (!el) return
 
-    setX(round(el.scrollLeft / (el.scrollWidth - el.clientWidth)))
-    setY(round(el.scrollTop / (el.scrollHeight - el.clientHeight)))
+    return {
+      x: round(el.scrollLeft / (el.scrollWidth - el.clientWidth)),
+      y: round(el.scrollTop / (el.scrollHeight - el.clientHeight))
+    }
   }
-
-  useMount(() => {
-    setPositions()
-  })
 
   useEventListener(
     target,
     'scroll',
     () => {
-      setPositions()
+      const newScrollValues = getPositions()
+      if (!newScrollValues) return
+
+      const { x, y } = newScrollValues
+      callback({ scrollX: x, scrollY: y })
     },
     {
       capture: false,
       passive: true
     }
   )
-
-  return { x, y }
 }

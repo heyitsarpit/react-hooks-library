54f71a97449971cfac9cece1b82c6cadd7a8c60e
Arpit Bharti <arpitbharti73@gmail.com>
2021-09-26 16:24:02 +0530

    npm publish automation script

---
+++ scripts/publish.ts
@@ -0,0 +1,25 @@
+import { execSync } from 'child_process'
+import consola from 'consola'
+import { join, resolve } from 'path'
+
+import { version } from '../package.json'
+import { findFunctions } from './utils/findFunctions'
+
+execSync('npm run build', { stdio: 'inherit' })
+
+let command = 'npm publish --access public'
+
+if (version.includes('beta')) command += ' --tag beta'
+
+const rootDir = resolve(__dirname, '..')
+const packagesDir = resolve(rootDir, 'packages')
+const packages = findFunctions(packagesDir)
+
+for (const pkg of packages) {
+  execSync(command, {
+    stdio: 'inherit',
+    cwd: join('packages', pkg, 'dist')
+  })
+
+  consola.success(`Published @react-hooks-library/${pkg}`)
+}

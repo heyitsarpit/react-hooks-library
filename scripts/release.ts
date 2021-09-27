aeda099afbd46998629e055f45d61bdf40d35c7b
Arpit Bharti <arpitbharti73@gmail.com>
2021-09-27 23:51:05 +0530

    sanitize naming for release and commits

--- scripts/release.ts
+++ scripts/release.ts
@@ -44,6 +44,6 @@ for (const pkg of packages) {
 }
 
 execSync('git add .', { stdio: 'inherit' })
-execSync(`git commit -m "release: v${version}"`, { stdio: 'inherit' })
+execSync(`git commit -m "v${version}"`, { stdio: 'inherit' })
 execSync(`git tag -a v${version} -m "v${version}"`, { stdio: 'inherit' })
 execSync(`git push --follow-tags`, { stdio: 'inherit' })

739649be364ea8da403d9b65b4799d42aceff3b0
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-08 11:59:43 +0530

    Update new fonts globally and update demo header

--- website/ui/docs/Playground.tsx
+++ website/ui/docs/Playground.tsx
@@ -4,8 +4,10 @@ type Props = { children: React.ReactNode }
 
 export function Playground({ children }: Props) {
   return (
-    <div className="bleed min-h-[16rem] polka px-12 py-2 top-[-2rem] border-b border-b-coolGray-200 dark:border-b-coolGray-900">
-      <div className="absolute opacity-30 top-2 right-5">DEMO</div>
+    <div className="bleed min-h-[16rem] polka px-12 pt-10 pb-2 top-[-2rem] border-b border-b-coolGray-200 dark:border-b-coolGray-900">
+      <div className="absolute text-sm opacity-30 top-10 right-5 md:text-base">
+        DEMO
+      </div>
       <div className="flex items-center justify-center min-h-[inherit]">
         {children}
       </div>

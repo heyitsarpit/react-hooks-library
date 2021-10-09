9df8e1d763d9d32e0861a4d80478783cc87934a3
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-09 17:16:07 +0530

    modify build routes to include description

--- website/routes.ts
+++ website/routes.ts
@@ -1,146 +1,174 @@
 // This is a auto generated file. Do not modify
 
-export type Route = { name: string; category: string; route: string }
+export type Route = { name: string; category: string; route: string; description: string }
 
 export const routes: Route[] = [
   {
     "name": "BreakPointHooks",
     "category": "browser",
-    "route": "/core/BreakPointHooks"
+    "route": "/core/BreakPointHooks",
+    "description": "Reaactive hooks and utilities to be used with user provided breakpoints."
   },
   {
     "name": "useActiveElement",
     "category": "browser",
-    "route": "/core/useActiveElement"
+    "route": "/core/useActiveElement",
+    "description": "Reactive document.activeElement"
   },
   {
     "name": "useClickOutside",
     "category": "sensors",
-    "route": "/core/useClickOutside"
+    "route": "/core/useClickOutside",
+    "description": "Listen for click outside an element, useful for modals and tooltips."
   },
   {
     "name": "useDebounce",
     "category": "utilities",
-    "route": "/core/useDebounce"
+    "route": "/core/useDebounce",
+    "description": "Used to debounce a quickly changing value. Will return the latest value after a specified amount of time."
   },
   {
     "name": "useEventListener",
     "category": "browser",
-    "route": "/core/useEventListener"
+    "route": "/core/useEventListener",
+    "description": "Register listener using addEventListener when mounting, and removeEventListener automatically when un-mounting."
   },
   {
     "name": "useFont",
     "category": "browser",
-    "route": "/core/useFont"
+    "route": "/core/useFont",
+    "description": "React FontFace, a hook to load fonts asynchronously"
   },
   {
     "name": "useHasMounted",
     "category": "state",
-    "route": "/core/useHasMounted"
+    "route": "/core/useHasMounted",
+    "description": "Hook that returns whether or not the component has mounted"
   },
   {
     "name": "useIntersectionObserver",
     "category": "browser",
-    "route": "/core/useIntersectionObserver"
+    "route": "/core/useIntersectionObserver",
+    "description": "Reactive intersection observer."
   },
   {
     "name": "useIsSupported",
     "category": "sensors",
-    "route": "/core/useIsSupported"
+    "route": "/core/useIsSupported",
+    "description": "Is a feature supported in the browser or not"
   },
   {
     "name": "useKeyStroke",
     "category": "sensors",
-    "route": "/core/useKeyStroke"
+    "route": "/core/useKeyStroke",
+    "description": "Listen for keyboard keys on keystroke"
   },
   {
     "name": "useLocation",
     "category": "browser",
-    "route": "/core/useLocation"
+    "route": "/core/useLocation",
+    "description": "Reactive browser location."
   },
   {
     "name": "useMediaQuery",
     "category": "browser",
-    "route": "/core/useMediaQuery"
+    "route": "/core/useMediaQuery",
+    "description": "Reactive media query hook that returns the truthy value of the media query."
   },
   {
     "name": "useMediaStream",
     "category": "sensors",
-    "route": "/core/useMediaStream"
+    "route": "/core/useMediaStream",
+    "description": "Reactive `mediaDevices.getUserMedia` streaming"
   },
   {
     "name": "useMount",
     "category": "lifecycle",
-    "route": "/core/useMount"
+    "route": "/core/useMount",
+    "description": "Run a function when a component is mounted."
   },
   {
     "name": "useMountSync",
     "category": "lifecycle",
-    "route": "/core/useMountSync"
+    "route": "/core/useMountSync",
+    "description": "Run a function synchronously when a component is mounted but after DOM is painted."
   },
   {
     "name": "useMutationObserver",
     "category": "browser",
-    "route": "/core/useMutationObserver"
+    "route": "/core/useMutationObserver",
+    "description": "Watch for changes being made to the DOM tree."
   },
   {
     "name": "useNetwork",
     "category": "sensors",
-    "route": "/core/useNetwork"
+    "route": "/core/useNetwork",
+    "description": "Reactive Network status"
   },
   {
     "name": "useOnline",
     "category": "sensors",
-    "route": "/core/useOnline"
+    "route": "/core/useOnline",
+    "description": "Reactive online status"
   },
   {
     "name": "usePreferredColorScheme",
     "category": "browser",
-    "route": "/core/usePreferredColorScheme"
+    "route": "/core/usePreferredColorScheme",
+    "description": "Reactive prefers color scheme."
   },
   {
     "name": "usePrevious",
     "category": "state",
-    "route": "/core/usePrevious"
+    "route": "/core/usePrevious",
+    "description": "Find previous value of a state"
   },
   {
     "name": "useScroll",
     "category": "sensors",
-    "route": "/core/useScroll"
+    "route": "/core/useScroll",
+    "description": "Reactive scroll values for a react ref or a dom node"
   },
   {
     "name": "useScrollIntoView",
     "category": "browser",
-    "route": "/core/useScrollIntoView"
+    "route": "/core/useScrollIntoView",
+    "description": "A hook to scroll an element into view on mounting"
   },
   {
     "name": "useStateHistory",
     "category": "state",
-    "route": "/core/useStateHistory"
+    "route": "/core/useStateHistory",
+    "description": "useState with built in undo and redo history control"
   },
   {
     "name": "useTitle",
     "category": "browser",
-    "route": "/core/useTitle"
+    "route": "/core/useTitle",
+    "description": "Reactive document title hook, set title or observe dom mutation reactively"
   },
   {
     "name": "useToggle",
     "category": "state",
-    "route": "/core/useToggle"
+    "route": "/core/useToggle",
+    "description": "A state toggle hook"
   },
   {
     "name": "useUnMount",
     "category": "lifecycle",
-    "route": "/core/useUnMount"
+    "route": "/core/useUnMount",
+    "description": "Run a function when component is unmounted."
   },
   {
     "name": "useWindowSize",
     "category": "sensors",
-    "route": "/core/useWindowSize"
+    "route": "/core/useWindowSize",
+    "description": "Reactive window size"
   },
   {
     "name": "utils",
     "category": "shared",
-    "route": "/shared/utils"
+    "route": "/shared/utils",
+    "description": "Common utils and helpers."
   }
 ]
--- website/scripts/buildRoutes.ts
+++ website/scripts/buildRoutes.ts
@@ -7,13 +7,18 @@ const indexPath = join(__dirname, '..', 'routes.ts')
 
 async function buildRoutes() {
   const functionMeta = (await getAllFunctionsMeta()).map(
-    ({ name, pkg, category }) => ({ name, category, route: `/${pkg}/${name}` })
+    ({ name, pkg, category, description }) => ({
+      name,
+      category,
+      route: `/${pkg}/${name}`,
+      description
+    })
   )
 
   const source = [
     `// This is a auto generated file. Do not modify\n\n`,
 
-    `export type Route = { name: string; category: string; route: string }\n\n`,
+    `export type Route = { name: string; category: string; route: string; description: string }\n\n`,
     `export const routes: Route[] = `,
     JSON.stringify(functionMeta, null, 2)
   ].join('')

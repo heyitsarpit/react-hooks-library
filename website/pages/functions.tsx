8429ff6f011f362d4527b573fb3c03219f9d4405
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-10 13:05:41 +0530

    Added SEO tags for /functions and /getting-started

--- website/pages/functions.tsx
+++ website/pages/functions.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { NextSeo } from 'next-seo'
 import { Fragment } from 'react'
 
 import type { Route } from '../routes'
@@ -18,25 +19,37 @@ routes.forEach((route) => {
 
 export default function Functions() {
   return (
-    <div className="p-8">
-      <h1>Functions</h1>
-      {Object.keys(groupedRoutes).map((category) => (
-        <Fragment key={category}>
-          <h2 className="text-lg font-bold capitalize text-txt-2">
-            {category}
-          </h2>
-          <ul className="flex flex-col gap-3 list-none">
-            {groupedRoutes[category].map(({ name, route, description }) => (
-              <li key={name}>
-                <Link href={route}>
-                  <a>{name}</a>
-                </Link>
-                <p>{description}</p>
-              </li>
-            ))}
-          </ul>
-        </Fragment>
-      ))}
-    </div>
+    <>
+      <NextSeo
+        title="Functions — React Hooks Library"
+        description="List of all functions for react hooks library"
+        canonical="https://react-hooks-library.vercel.app/functions"
+        openGraph={{
+          url: 'https://react-hooks-library.vercel.app/functions',
+          title: 'Functions — React Hooks Library',
+          description: 'List of all functions for react hooks library'
+        }}
+      />
+      <div className="p-8">
+        <h1>Functions</h1>
+        {Object.keys(groupedRoutes).map((category) => (
+          <Fragment key={category}>
+            <h2 className="text-lg font-bold capitalize text-txt-2">
+              {category}
+            </h2>
+            <ul className="flex flex-col gap-3 list-none">
+              {groupedRoutes[category].map(({ name, route, description }) => (
+                <li key={name}>
+                  <Link href={route}>
+                    <a>{name}</a>
+                  </Link>
+                  <p>{description}</p>
+                </li>
+              ))}
+            </ul>
+          </Fragment>
+        ))}
+      </div>
+    </>
   )
 }
--- website/pages/getting-started.tsx
+++ website/pages/getting-started.tsx
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import { getMDXComponent } from 'mdx-bundler/client'
 import { InferGetStaticPropsType } from 'next'
+import { NextSeo } from 'next-seo'
 import path from 'path'
 import { useMemo } from 'react'
 import { mdxComponents } from 'ui/docs/mdxComponents'
@@ -24,8 +25,20 @@ export default function GettingStarted({ code }: Props) {
   const CodeComponent = useMemo(() => getMDXComponent(code), [code])
 
   return (
-    <div>
-      <CodeComponent components={mdxComponents as any} />
-    </div>
+    <>
+      <NextSeo
+        title="Get Started — React Hooks Library"
+        description="Guide for react hooks library"
+        canonical="https://react-hooks-library.vercel.app/getting-started"
+        openGraph={{
+          url: 'https://react-hooks-library.vercel.app/getting-started',
+          title: 'Get Started — React Hooks Library',
+          description: 'Guide for react hooks library'
+        }}
+      />
+      <div className="p-8">
+        <CodeComponent components={mdxComponents as any} />
+      </div>
+    </>
   )
 }

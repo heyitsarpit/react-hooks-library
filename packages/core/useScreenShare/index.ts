f0a0ca1ce1fa4a65ddf1b8ef82120e92e9afefaf
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-18 21:06:51 +0530

    Added new hook useScreenShare

---
+++ packages/core/useScreenShare/index.ts
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from 'react'
+
+import { _navigator } from '../_ssr.config'
+import { useIsSupported } from '../useIsSupported'
+
+export interface UseScreenShareOptions {
+  /**
+   * If the stream video media constraints
+   *
+   * @default true
+   */
+  video?: boolean | MediaTrackConstraints | undefined
+  /**
+   * If the stream audio media constraints
+   *
+   * @default true
+   */
+  audio?: boolean | MediaTrackConstraints | undefined
+}
+
+/**
+ * Reactive screen sharing
+ *
+ * @param options
+ *
+ * @see https://react-hooks-library.vercel.app/core/useScreenShare
+ */
+export function useScreenShare(options: UseScreenShareOptions = {}) {
+  const { audio = true, video = true } = options
+  const isSupported = useIsSupported(
+    () => !!_navigator?.mediaDevices?.getDisplayMedia
+  )
+
+  const stream = useRef<MediaStream | null>(null)
+  const ref = useRef<HTMLVideoElement | null>(null)
+
+  const [isPlaying, setPlaying] = useState(false)
+
+  async function play() {
+    if (!isSupported || !ref.current) return
+
+    stream.current =
+      (await _navigator?.mediaDevices.getDisplayMedia({
+        audio,
+        video
+      })) ?? null
+
+    setPlaying(true)
+
+    return stream.current
+  }
+
+  async function stop() {
+    stream.current?.getTracks().forEach((t) => t.stop())
+    stream.current = null
+    setPlaying(false)
+  }
+
+  useEffect(() => {
+    if (!ref.current) return
+
+    ref.current.srcObject = stream.current
+
+    // Handle os native stop screen sharing buttons
+    stream.current?.getVideoTracks()[0].addEventListener('ended', stop)
+  }, [isPlaying])
+
+  return {
+    isSupported,
+    isPlaying,
+    ref,
+    stream,
+    play,
+    stop
+  }
+}

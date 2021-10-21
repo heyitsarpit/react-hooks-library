58d1761cac2c175880c4326e776a7e009b3776b7
Arpit Bharti <arpitbharti73@gmail.com>
2021-10-21 20:13:06 +0530

    Added new serialize and deserialize options for useLocalStorage

--- packages/core/useLocalStorage/index.ts
+++ packages/core/useLocalStorage/index.ts
@@ -4,24 +4,43 @@ import { useState } from 'react'
 
 import { useMount } from '../useMount'
 
+export type UseLocalStorageOptions = {
+  /**
+   * Function for converting to string.
+   *
+   * @default JSON.stringify
+   */
+  serialize: (value: unknown) => string
+
+  /**
+   * Function to convert stored string to object value.
+   *
+   * @default JSON.parse
+   */
+  deserialize: (value: string) => unknown
+}
+
 /**
  * Modified `useState` hook that syncs with localStorage.
  *
  * @param key
  * @param initialValue
+ * @param options
  *
  * @see https://react-hooks-library.vercel.app/core/useLocalStorage
  */
 export function useLocalStorage<T>(
   key: string,
-  initialValue: T
+  initialValue: T,
+  options?: UseLocalStorageOptions
 ): [T, Dispatch<SetStateAction<T>>] {
   const [storedValue, setStoredValue] = useState(initialValue)
+  const { deserialize = JSON.parse, serialize = JSON.stringify } = options || {}
 
   useMount(() => {
     try {
       const item = localStorage.getItem(key)
-      item && setStoredValue(JSON.parse(item))
+      item && setStoredValue(deserialize(item))
     } catch (error) {
       console.error(error)
     }
@@ -32,7 +51,7 @@ export function useLocalStorage<T>(
       const valueToStore = isFunction(value) ? value(storedValue) : value
 
       setStoredValue(valueToStore)
-      localStorage.setItem(key, JSON.stringify(valueToStore))
+      localStorage.setItem(key, serialize(valueToStore))
     } catch (error) {
       console.error(error)
     }

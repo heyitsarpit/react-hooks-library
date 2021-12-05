ca921458405a42c268973af700bb267258472317
Arpit Bharti <arpitbharti73@gmail.com>
2021-12-05 17:54:28 +0530

    Added useMouse demo

---
+++ packages/core/useMouse/demo.tsx
@@ -0,0 +1,18 @@
+import { useMouse } from '.'
+
+export function Demo() {
+  const { x, y } = useMouse({ type: 'page' })
+
+  return (
+    <div>
+      <div className="flex flex-col min-w-[10rem] gap-4">
+        <span>
+          x: <span className="active pill">{x}</span>
+        </span>
+        <span>
+          y: <span className="active pill">{y}</span>
+        </span>
+      </div>
+    </div>
+  )
+}

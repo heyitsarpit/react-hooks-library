cdfebac8ff4cc8b384f3ef5fccde4d2ea66875b2
Arpit Bharti <arpitbharti73@gmail.com>
2022-03-27 18:01:26 +0530

    feat(useAsyncCallback): added isSuccess state

--- packages/core/useAsyncCallback/index.ts
+++ packages/core/useAsyncCallback/index.ts
@@ -3,6 +3,7 @@ import { useCallback, useState } from 'react'
 export type UseAsyncState<T> = {
   data: T | undefined
   error: boolean
+  isSuccess: boolean
   isLoading: boolean
 }
 
@@ -15,27 +16,29 @@ export type UseAsyncState<T> = {
 export function useAsyncCallback<Args extends unknown[], ResolvedType>(
   callback: (...args: Args) => Promise<ResolvedType>
 ): [UseAsyncState<ResolvedType>, (...args: Args) => Promise<ResolvedType>] {
-  const [isLoading, setLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
   const [error, setError] = useState(false)
   const [data, setData] = useState<ResolvedType>()
 
   const _callback = useCallback(
     async (...args: Args) => {
       try {
-        setLoading(true)
+        setIsLoading(true)
         const results = await callback(...args)
         setData(results)
+        setIsSuccess(true)
 
         return results
       } catch (e) {
         setError(true)
         throw e
       } finally {
-        setLoading(false)
+        setIsLoading(false)
       }
     },
     [callback]
   )
 
-  return [{ data, error, isLoading }, _callback]
+  return [{ data, error, isLoading, isSuccess }, _callback]
 }

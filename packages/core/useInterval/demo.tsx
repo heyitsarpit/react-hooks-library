10e856b404ed698baec2c7b252a492d2e3cf3316
Arpit Bharti <arpitbharti73@gmail.com>
2022-09-15 22:03:17 +0530

    docs(useInterval): update docs and demo

--- packages/core/useInterval/demo.tsx
+++ packages/core/useInterval/demo.tsx
@@ -4,14 +4,20 @@ import { useInterval } from '.'
 
 export function Demo() {
   const [count, setCount] = useState(0)
+  const [paused, setPaused] = useState(false)
 
   const increment = useCallback(() => setCount((c) => c + 1), [])
 
-  useInterval(increment, 1000)
+  useInterval(increment, 1000, { paused })
 
   return (
-    <div>
-      Count - <span className="pill active">{count}</span>
+    <div className="flex flex-col gap-4">
+      <div>
+        Count - <span className="pill active">{count}</span>
+      </div>
+      <button onClick={() => setPaused((p) => !p)}>
+        {paused ? 'Unpause' : 'Pause'}
+      </button>
     </div>
   )
 }
